5b353870b33893a9d60976dd27a29ddfde933366
YuTomida <yutomida@outlook.com>
2021-11-08 21:39:04 -0300

    criada rotas das vacinas

--- project/server/main/views.py
+++ project/server/main/views.py
@@ -15,3 +15,28 @@ def home():
 @main_blueprint.route("/about/")
 def about():
     return render_template("main/about.html")
+
+
+@main_blueprint.route("/adolescentes/")
+def adolescentes():
+    return render_template("calendario/adolescentes.html")
+
+
+@main_blueprint.route("/adultos/")
+def adultos():
+    return render_template("calendario/adultos.html")
+
+
+@main_blueprint.route("/criancas/")
+def criancas():
+    return render_template("calendario/criancas.html")
+
+
+@main_blueprint.route("/gestantes/")
+def gestantes():
+    return render_template("calendario/gestantes.html")
+
+
+@main_blueprint.route("/idosos/")
+def idosos():
+    return render_template("calendario/idosos.html")        
