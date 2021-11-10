09adaa4369143eb6648b4deb49e92d263b3eeb8a
Rodrigo Prado de Jesus <royopa@gmail.com>
2021-11-09 21:57:53 -0300

    finalizado design paginas e logica de processamento formulario

--- project/server/main/forms.py
+++ project/server/main/forms.py
@@ -1,12 +1,14 @@
 # project/server/main/forms.py
 from flask_wtf import FlaskForm
-from wtforms import DateField, PasswordField, SelectField, StringField
+from wtforms import PasswordField, SelectField, StringField
+from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 
 class ConsultaCalendarioForm(FlaskForm):
-    nome = StringField(u'Nome', [DataRequired(), Length(min=3, max=100)])
     data_nascimento = DateField(
         "Data de nascimento", [DataRequired()], format="%d/%m/%Y")
-    is_gestante = SelectField(u'Você está gestante?', choices=[(
-        None, 'Você está gestante?'), (True, 'Sim'), (False, 'Não')])
+    is_gestante = SelectField(u'Você está gestante?', choices=[
+        ('0', 'Não'),
+        ('1', 'Sim')
+    ])
--- project/server/main/views.py
+++ project/server/main/views.py
@@ -1,7 +1,9 @@
 # project/server/main/views.py
+from datetime import datetime
 
-
-from flask import Blueprint, flash, redirect, render_template, request, url_for
+from dateutil.relativedelta import relativedelta
+from flask import (Blueprint, Markup, flash, redirect, render_template,
+                   request, url_for)
 from project.server.main.forms import ConsultaCalendarioForm
 
 main_blueprint = Blueprint("main", __name__)
@@ -9,7 +11,12 @@ main_blueprint = Blueprint("main", __name__)
 
 @main_blueprint.route("/")
 def home():
-    return render_template("main/home.html")
+    return redirect(url_for("main.consulta_calendario"))
+
+
+@main_blueprint.route("/sobre")
+def sobre():
+    return render_template("main/sobre.html")
 
 
 @main_blueprint.route("/adolescentes")
@@ -40,16 +47,56 @@ def idosos():
 @main_blueprint.route("/consulta_calendario", methods=["GET", "POST"])
 def consulta_calendario():
     form = ConsultaCalendarioForm(request.form)
+
     if form.validate_on_submit():
-        nome = form.nome.data
         data_nascimento = form.data_nascimento.data
         is_gestante = form.is_gestante.data
+        hoje = datetime.today().date()
+        anos = relativedelta(hoje, data_nascimento).years
 
-        print(nome, data_nascimento, is_gestante)
+        # mensagem de alerta para as gestantes
+        if is_gestante == '1':
+            mensagem = '''
+            Atenção!  De acordo com o Ministério da Saúde, há vacinas que gestantes não podem tomar. Verifique com seu médico.
+            '''
+            mensagem = Markup(mensagem)
+            flash(mensagem, "danger")
 
-        # db.session.add(user)
+        # criança - >=0 and 4 anos
+        if anos >= 0 and anos <= 4:
+            return render_template("calendario/criancas.html", is_gestante=is_gestante)
 
-        flash("Direcionando para o calendário de idosos.", "success")
-        return redirect(url_for("main.idosos"))
+        if anos > 4 and anos < 9:
+            # criança sem vacina - +4 e <9
+            mensagem = '''
+            Para essa faixa etária não há vacinas disponibilizadas obrigatórias de acordo com o ministério da Saúde.
+            </br>
+            Dicas:
+            </br>
+            - Consulte o posto de vacinação mais próximo para verificar se há campanhas locais de imunização.</br>
+            -  Veja o calendário anterior se há alguma vacina faltante para imunização completa no quadro abaixo.            
+            '''
+            mensagem = Markup(mensagem)
+            flash(mensagem, "warning")
+            return render_template("calendario/criancas.html", is_gestante=is_gestante)
+
+        # adolescente - 9 a 19 anos
+        if anos >= 9 and anos <= 19:
+            return render_template("calendario/adolescentes.html", is_gestante=is_gestante)
+
+        # adulto - 20 A 59 anos
+        if anos >= 20 and anos <= 59:
+            return render_template("calendario/adultos.html", is_gestante=is_gestante)
+
+        # idoso += 60
+        if anos >= 60:
+            return render_template("calendario/idosos.html", is_gestante=is_gestante)
+
+        # Tabela
+        # nome_grupo       caminho_pagina
+        # adolescentes     calendario/adolescentes.html
+        # criancas         calendario//criancas.html
+
+        print(anos)
 
     return render_template("main/home.html", form=form)
