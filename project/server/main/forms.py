88024e148f6fa8ebd3b6f4031fef9a48f048944b
Rodrigo Prado de Jesus <royopa@gmail.com>
2021-11-10 13:25:41 -0300

    ajustada validaçao campo data

--- project/server/main/forms.py
+++ project/server/main/forms.py
@@ -7,7 +7,7 @@ from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 class ConsultaCalendarioForm(FlaskForm):
     data_nascimento = DateField(
-        "Data de nascimento", [DataRequired()], format="%d/%m/%Y")
+        "Data de nascimento", [DataRequired()])
     is_gestante = SelectField(u'Você está gestante?', choices=[
         ('0', 'Não'),
         ('1', 'Sim')
