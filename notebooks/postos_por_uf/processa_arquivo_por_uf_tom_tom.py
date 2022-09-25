176414c2599525f996b7b6426f30e8cb58ad1524
Rodrigo Prado de Jesus <royopa@gmail.com>
2022-09-25 17:30:26 -0300

    adicionado programa tomtom

---
+++ notebooks/postos_por_uf/processa_arquivo_por_uf_tom_tom.py
@@ -0,0 +1,62 @@
+import os
+import sys
+
+import pandas as pd
+from dotenv import load_dotenv
+from geopy.extra.rate_limiter import RateLimiter
+from geopy.geocoders import HereV7, Nominatim, TomTom
+
+load_dotenv()  # take environment variables from .env.
+
+print("This is the name of the script: ", sys.argv[0])
+print("Number of arguments: ", len(sys.argv))
+print("The arguments are: ", str(sys.argv))
+
+ufs = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG',
+       'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR',
+       'RS', 'SC', 'SE', 'SP', 'TO']
+
+if len(sys.argv) > 1:
+    ufs = [sys.argv[1].upper()]
+
+print(ufs)
+
+
+def get_location(row_df):
+    # Geolocators
+    geolocator = TomTom(api_key='wvuAFmBut64DLjQqey19XtfuMXZilzbj', user_agent="None")
+    geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
+
+    # pula os registros que já possuem dados de localização
+    if type(row_df['address']) is str:
+        return row_df['latitude'], row_df['longitude'], row_df['address'], row_df['point']
+
+    # inicializa a variável que receberá o retorno das APIs de localização
+    location = None
+    print(' ', row_df['ENDERECO_COMPLETO'])
+    try:
+        # tenta fazer a localização usando o Nominatim
+        location = geolocator.geocode(row_df['ENDERECO_COMPLETO'])
+        if not location:
+            print(None)
+            return None, None, None, None
+    except Exception as e:
+        print(f' Erro: {e}')
+        print(None)
+        return None, None, None, None
+
+    print(location.latitude)
+    return location.latitude, location.longitude, location.address, location.point
+
+
+for uf in ufs:
+    file_path = os.path.join(f'postos_saude_brasil_{uf}.csv')
+    df = pd.read_csv(file_path)
+
+    tamanho = len(df)
+    print(f'Processando {str(tamanho).zfill(5)} registros')
+    df['latitude'], df['longitude'], df['address'], df['point'] = zip(
+      *df.apply(get_location, axis=1))
+
+    df.to_csv(file_path, index=False)
+    print('Arquivo de saída atualizado com sucesso.', file_path)
