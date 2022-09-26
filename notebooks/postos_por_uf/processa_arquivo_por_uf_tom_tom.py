bd50903d59aa7c9cff3ba05650c6285ba5de0914
Rodrigo Prado de Jesus <royopa@gmail.com>
2022-09-26 10:42:49 -0300

    adicionado novos arquivos apos processamento tomtom

--- notebooks/postos_por_uf/processa_arquivo_por_uf_tom_tom.py
+++ notebooks/postos_por_uf/processa_arquivo_por_uf_tom_tom.py
@@ -16,6 +16,11 @@ ufs = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG',
        'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR',
        'RS', 'SC', 'SE', 'SP', 'TO']
 
+# lista para remover registros que já passaram por todas UBs
+ufs = ['BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG',
+       'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN',
+       'RS', 'SC', 'SE', 'SP']
+
 if len(sys.argv) > 1:
     ufs = [sys.argv[1].upper()]
 
