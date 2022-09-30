39e0da0aaf2c72f6c4a19839e76aff2a0b30491a
Rodrigo Prado de Jesus <royopa@gmail.com>
2022-09-29 22:16:44 -0300

    simulada apis de consulta

--- project/server/api_app/views.py
+++ project/server/api_app/views.py
@@ -1,6 +1,7 @@
 # project/server/api_app/views.py
 from flask_restx import Api, Resource
 from flask import (Blueprint)
+from flask_restx import reqparse
 from dotenv import load_dotenv
 load_dotenv()  # take environment variables from .env.
 
@@ -8,13 +9,90 @@ api_blueprint = Blueprint("api", __name__)
 api = Api(api_blueprint)
 
 
-@api.route("/hello")
-class HelloWorld(Resource):
+parser = reqparse.RequestParser()
+parser.add_argument(
+    'latitude',
+    type=float,
+    required=True,
+    help="Latitude não pode ser vazio!",
+    location='args')
+parser.add_argument(
+    'longitude',
+    type=float,
+    required=True,
+    help="Longitude não pode ser vazio!",
+    location='args')
+parser.add_argument(
+    'quantidade',
+    type=int,
+    required=True,
+    help="Quantidade não pode ser vazio!",
+    location='args')
+
+
+@api.route(
+  "/unidades_saude")
+class UnidadesSaude(Resource):
+    @api.expect(parser)
     def get(self):
-        return {'hello': 'world'}
+        args = parser.parse_args()
 
-    def post(self):
-        return {'hello': 'world'}
+        args['latitude']
+        args['longitude']
+        args['quantidade']
 
-    def delete(self):
-        return {'hello': 'world'}
+        lista_unidades_saude = []
+        unidade = {
+          'CO_CNES': '253699',
+          'NOME': 'UBS SEM TUPIUTUBA',
+          'LOGRADOURO': 'RUA MARA SILVA',
+          'NUMERO': '25',
+          'BAIRRO': 'VILA FORMOSA',
+          'MUNICIPIO': 'SAO PAULO',
+          'UF': 'SP',
+          'LATITUDE': -25.3623,
+          'LONGITUDE': 69.3632,
+          'CEP': '03382-130',
+        }
+        lista_unidades_saude.append(unidade)
+        unidade = {
+          'CO_CNES': '253699',
+          'NOME': 'UBS SEM TUPIUTUBA',
+          'LOGRADOURO': 'RUA MARA SILVA',
+          'NUMERO': '25',
+          'BAIRRO': 'VILA FORMOSA',
+          'MUNICIPIO': 'SAO PAULO',
+          'UF': 'SP',
+          'LATITUDE': -25.3623,
+          'LONGITUDE': 69.3632,
+          'CEP': '03382-130',
+        }
+        lista_unidades_saude.append(unidade)
+        unidade = {
+          'CO_CNES': '253699',
+          'NOME': 'UBS SEM TUPIUTUBA',
+          'LOGRADOURO': 'RUA MARA SILVA',
+          'NUMERO': '25',
+          'BAIRRO': 'VILA FORMOSA',
+          'MUNICIPIO': 'SAO PAULO',
+          'UF': 'SP',
+          'LATITUDE': -25.3623,
+          'LONGITUDE': 69.3632,
+          'CEP': '03382-130',
+        }
+        lista_unidades_saude.append(unidade)
+        unidade = {
+          'CO_CNES': '253699',
+          'NOME': 'UBS SEM TUPIUTUBA',
+          'LOGRADOURO': 'RUA MARA SILVA',
+          'NUMERO': '25',
+          'BAIRRO': 'VILA FORMOSA',
+          'MUNICIPIO': 'SAO PAULO',
+          'UF': 'SP',
+          'LATITUDE': -25.3623,
+          'LONGITUDE': 69.3632,
+          'CEP': '03382-130',
+        }
+        lista_unidades_saude.append(unidade)
+
+        return {"resultado": lista_unidades_saude}
